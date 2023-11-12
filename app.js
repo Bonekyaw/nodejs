345e549b45803d9fd138620ac4c6e6eb39afaa79
bonekyaw <phonenai2014@gmail.com>
2023-11-12 23:35:31 +0630

    req res buffer filewrite redirect

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
+const http = require('http');
 const fs = require('fs');
 
-fs.writeFileSync('hello.txt', 'Hello From node fs');
+const server = http.createServer((req, res) => {
+    const method = req.method;
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write("<html>");
+        res.write("<head><title>Hey Node</title></head>");
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Save</button></form></body>");
+        res.write("</html>");
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+
+            fs.writeFileSync('message.txt', message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write("<html>");
+    res.write("<head><title>Hey Node</title></head>");
+    res.write("<body><h1>Welcome to Node js</h1></body>");
+    res.write("</html>");
+    res.end();
+
+});
+
+server.listen(3000);
+
+
+
+
+
+
+// const names = (...args) => args;
+// console.log(names(1,2,3));
