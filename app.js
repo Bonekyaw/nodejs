5f0441c1cc30e0b8977893bb61e8f43d4d19231a
bonekyaw <phonenai2014@gmail.com>
2023-11-13 21:01:25 +0630

    express simple small app

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
+app.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(3000);
+app.get('/add-product', (req, res, next) => {
+    res.send("<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Save</button></form>");
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from my Home</h1>');
+})
+
+app.listen(3000);
 
 
 
