ca4333c7e50cade9580d8ac580a0f7ae7641db82
bonekyaw <phonenai2014@gmail.com>
2023-11-13 22:15:15 +0630

    Codesplitting router, prefix url & 404 not found

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/add-product', (req, res, next) => {
-    res.send("<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Save</button></form>");
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
 });
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from my Home</h1>');
-})
 
 app.listen(3000);
 
