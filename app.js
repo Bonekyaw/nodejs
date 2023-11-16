19dc4791fb36e78f87cc45a922b33ea6f434c3f3
bonekyaw <phonenai2014@gmail.com>
2023-11-16 14:51:53 +0630

    ejs include

--- app.js
+++ app.js
@@ -3,31 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const rootDir = require('./util/path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const app = express();
-
-app.set('view engine', 'pug');
-app.set('views', 'views');
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/' });
 });
 
 app.listen(3000);
-
-
-
-
-
-
-// const names = (...args) => args;
-// console.log(names(1,2,3));
--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,25 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
-router.get('/product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/product' });
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
-router.post('/product', (req, res, next) => {
-    console.log(req.body);
-    products.push({title: req.body.title});
-    res.redirect('/');
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
 
 exports.routes = router;
